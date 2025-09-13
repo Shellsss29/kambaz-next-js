3bbbfdee8cc80eabc424d8bf4edad6e911cdb6dc
Shailly Bhati <shailly@Shaillys-MacBook-Pro.local>
2025-09-13 14:12:41 -0400

    Redirecting Labs main page to Lab1

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -1,27 +1,5 @@
-import Link from "next/link";
-export default function Labs() {
-    return (
-        <div id="wd-labs">
-            <header>
-                <h3>Student: Shailly Bhati</h3>
-                <a href="https://github.com/Shellsss29/kambaz-next-js" id="wd-github" target="_blank">Click here to access Github Repository</a>
-                <hr />
-            </header>
-            <h1>Labs</h1>
-            <ul>
-            <li>
-                <Link href="/Labs/Lab1" id="wd-lab1-link">
-                Lab 1: HTML Examples </Link>
-            </li>
-            <li>
-                <Link href="/Labs/Lab2" id="wd-lab2-link">
-                Lab 2: CSS Basics </Link>
-            </li>
-            <li>
-                <Link href="/Labs/Lab3" id="wd-lab3-link">
-                Lab 3: JavaScript Fundamentals </Link>
-            </li>
-            </ul>
-        </div>
-);}
+import { redirect } from "next/navigation";
 
+export default function Home() {
+    redirect("/Labs/Lab1");
+}
